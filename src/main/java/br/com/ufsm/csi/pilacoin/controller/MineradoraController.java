7b1b12276fe33fcf99d4b5eecc3d142c4381892b
ewerbe <ewertonbarbos@gmail.com>
2023-11-16 18:57:16 -0300

    Inserindo JPA.

---
+++ src/main/java/br/com/ufsm/csi/pilacoin/controller/MineradoraController.java
@@ -0,0 +1,22 @@
+//package br.com.ufsm.csi.pilacoin.controller;
+//
+//import br.com.ufsm.csi.pilacoin.service.MineradoraService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//
+//@Controller
+//public class MineradoraController {
+//
+//    @Autowired
+//    private static MineradoraService mineradoraService;
+//
+//    public static void main(String[] args) {
+////
+////        @Autowired
+////        private MineradoraService mineradoraService;
+//
+//         //MineradoraService mineradoraService = new MineradoraService();
+//        mineradoraService.mineracaoAtiva = Boolean.TRUE;
+//         mineradoraService.minerarPilaCoin();
+//    }
+//}
