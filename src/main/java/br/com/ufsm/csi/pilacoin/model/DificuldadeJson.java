b02ab8565af33e4763acd0b7df8cc1dbc4f7369a
ewerbe <ewertonbarbos@gmail.com>
2023-11-16 18:51:50 -0300

    Datas refatoradas para String, para facilitar persistência.

--- src/main/java/br/com/ufsm/csi/pilacoin/model/DificuldadeJson.java
+++ src/main/java/br/com/ufsm/csi/pilacoin/model/DificuldadeJson.java
@@ -1,7 +1,9 @@
 package br.com.ufsm.csi.pilacoin.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,9 +17,15 @@ import java.util.Date;
 @NoArgsConstructor
 @JsonPropertyOrder(alphabetic = true)
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@Entity
 public class DificuldadeJson {
 
+    @Id
+    @SequenceGenerator(name = "seq_dificuldade", sequenceName = "seq_dificuldade", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_dificuldade")
+    @JsonIgnore
+    private Long id;
     private String dificuldade;
-    private Date inicio;
-    private Date validadeFinal;
+    private String inicio;
+    private String validadeFinal;
 }
