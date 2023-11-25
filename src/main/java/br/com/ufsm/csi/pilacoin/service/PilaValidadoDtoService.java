ceb5b23ad7d955ed157a0e0a58f87816bfa4a47c
ewerbe <ewertonbarbos@gmail.com>
2023-11-25 18:51:54 -0300

    Método para listar pilas validados persistidos no banco.

--- src/main/java/br/com/ufsm/csi/pilacoin/service/PilaValidadoDtoService.java
+++ src/main/java/br/com/ufsm/csi/pilacoin/service/PilaValidadoDtoService.java
@@ -5,6 +5,8 @@ import br.com.ufsm.csi.pilacoin.repository.PilaValidadoDtoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class PilaValidadoDtoService {
 
@@ -13,5 +15,9 @@ public class PilaValidadoDtoService {
 
     public void save(PilaValidadoDto pilaValidadoDto) {
         pilaValidadoDtoRepository.save(pilaValidadoDto);
+    }
+
+    public List<PilaValidadoDto> findAll() {
+        return pilaValidadoDtoRepository.findAll();
     }
 }
