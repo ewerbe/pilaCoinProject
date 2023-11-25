5394eb59fffd54dcc217fc4525f1d46a1b0c22bd
ewerbe <ewertonbarbos@gmail.com>
2023-11-25 18:53:11 -0300

    Endpoint para listar validados de outros usuários.

--- src/main/java/br/com/ufsm/csi/pilacoin/controller/PilaCoinController.java
+++ src/main/java/br/com/ufsm/csi/pilacoin/controller/PilaCoinController.java
@@ -1,8 +1,10 @@
 package br.com.ufsm.csi.pilacoin.controller;
 
 
+import br.com.ufsm.csi.pilacoin.dto.PilaValidadoDto;
 import br.com.ufsm.csi.pilacoin.model.PilaCoinJson;
 import br.com.ufsm.csi.pilacoin.service.PilaCoinJsonService;
+import br.com.ufsm.csi.pilacoin.service.PilaValidadoDtoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,11 +17,13 @@ public class PilaCoinController {
 
     @Autowired
     private PilaCoinJsonService pilaCoinJsonService;
+    @Autowired
+    private PilaValidadoDtoService pilaValidadoDtoService;
 
     //Endpoint pra lista os pilas minerados. Estes pilas ainda não são válidos para transação,
     // então: não contar como Saldo.
-    @GetMapping("/pilas-minerados")
-        private List<PilaCoinJson> getPilasMinerados() {
+    @GetMapping("/pilas/minerados")
+        private List<PilaCoinJson> getMinerados() {
         List<PilaCoinJson> minerados = new LinkedList<>();
         try{
             minerados = pilaCoinJsonService.findAll();
@@ -28,4 +32,15 @@ public class PilaCoinController {
         }
         return minerados;
         }
+
+    @GetMapping("/pilas/validados/outros")
+    private List<PilaValidadoDto> getValidadosOutros() {
+        List<PilaValidadoDto> validadosOutros = new LinkedList<>();
+        try{
+            validadosOutros = pilaValidadoDtoService.findAll();
+        }catch (Exception e ){
+            e.printStackTrace();
+        }
+        return validadosOutros;
+    }
 }
