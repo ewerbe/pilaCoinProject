01b88229956639bca653a1fee5372daa6617e025
ewerbe <ewertonbarbos@gmail.com>
2023-11-28 23:22:22 -0300

    Query para pegar a última dificuldade salva em banco. REVER.

--- src/main/java/br/com/ufsm/csi/pilacoin/repository/DificuldadeRepository.java
+++ src/main/java/br/com/ufsm/csi/pilacoin/repository/DificuldadeRepository.java
@@ -2,6 +2,10 @@ package br.com.ufsm.csi.pilacoin.repository;
 
 import br.com.ufsm.csi.pilacoin.model.DificuldadeJson;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface DificuldadeRepository extends JpaRepository<DificuldadeJson, Long> {
+
+    @Query("SELECT d FROM DificuldadeJson d ORDER BY d.dificuldade_json_seq DESC")
+    DificuldadeJson getLastDificuldade();
 }
