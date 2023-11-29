94498b680ca7546e481cc0655efb5875d5c428ad
ewerbe <ewertonbarbos@gmail.com>
2023-11-28 23:22:47 -0300

    Método para pegar a última dificuldade salva em banco. REVER.

--- src/main/java/br/com/ufsm/csi/pilacoin/service/DificuldadeService.java
+++ src/main/java/br/com/ufsm/csi/pilacoin/service/DificuldadeService.java
@@ -8,16 +8,15 @@ import org.springframework.stereotype.Service;
 @Service
 public class DificuldadeService {
 
-//    private void divulgaDificuldade() {
-//        SecureRandom secureRandom = new SecureRandom();
-//        byte[] bArray = new byte[267/8];
-//        secureRandom.nextBytes(bArray);
-//    }
     @Autowired
     private DificuldadeRepository dificuldadeRepository;
 
     public void save(DificuldadeJson dificuldadeJson) {
         dificuldadeRepository.save(dificuldadeJson);
+    }
+
+    public DificuldadeJson getLastDificuldade() {
+        return dificuldadeRepository.getLastDificuldade();
     }
 }
 
