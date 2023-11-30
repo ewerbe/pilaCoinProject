486c3859d3ca1915eeb158f357d7e7bd732d32e6
ewerbe <ewertonbarbos@gmail.com>
2023-11-30 00:01:37 -0300

    Refatorada consulta pra trazer a última dificuldade do BD.

--- src/main/java/br/com/ufsm/csi/pilacoin/repository/DificuldadeRepository.java
+++ src/main/java/br/com/ufsm/csi/pilacoin/repository/DificuldadeRepository.java
@@ -6,6 +6,9 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface DificuldadeRepository extends JpaRepository<DificuldadeJson, Long> {
 
-    @Query("SELECT d FROM DificuldadeJson d ORDER BY d.dificuldade_json_seq DESC")
+    @Query("SELECT d FROM DificuldadeJson d WHERE d.dificuldade_json_seq = (SELECT MAX(d2.dificuldade_json_seq) FROM DificuldadeJson d2)")
     DificuldadeJson getLastDificuldade();
+
+    //@Query("SELECT d FROM DificuldadeJson d WHERE d.dificuldade_json_seq = (SELECT MAX(d2.dificuldade_json_seq) FROM DificuldadeJson d2)")
+    //DificuldadeJson getLastDificuldade();
 }
