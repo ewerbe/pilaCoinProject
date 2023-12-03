c404c2a8a4caf17be0b053723c4f3b46938bb521
ewerbe <ewertonbarbos@gmail.com>
2023-12-03 20:31:49 -0300

    Classe refatorada. Retirada a persistência.

--- src/main/java/br/com/ufsm/csi/pilacoin/model/Query.java
+++ src/main/java/br/com/ufsm/csi/pilacoin/model/Query.java
@@ -2,9 +2,6 @@ package br.com.ufsm.csi.pilacoin.model;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,16 +13,17 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @JsonPropertyOrder(alphabetic = true)
 @JsonInclude(JsonInclude.Include.NON_NULL)
-@Entity
 public class Query {
 
-    @Id
-    @GeneratedValue
-    private Long id;
     private Long idQuery;
-    private String tipoQuery;
+    private String nomeUsuario; //nome do usuário para saber qual a fila de resposta.
+    private TypeQuery tipoQuery; //"USUARIOS", "PILA" OU "BLOCOS"
     private String status; //não obrigatório. pode ser usado para filtrar os resultados por status do pila ou do bloco.
     private String usuarioMinerador; //não obrigatório. pode ser usado para filtrar os resultados por nome de usuário;
     private String nonce; //não obrigatório. pode ser usado para filtrar os resultados pelo nonce;
     private Long idBloco; //não obrigatório. pode ser usado para filtrar os resultados pelo seu número.
+
+    public enum TypeQuery {
+        USUARIOS, PILA, BLOCO
+    }
 }
