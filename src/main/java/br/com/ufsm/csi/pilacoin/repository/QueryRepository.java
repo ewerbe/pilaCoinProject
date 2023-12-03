3c54495e5320dd372d6fd91e0bb00822dae38863
ewerbe <ewertonbarbos@gmail.com>
2023-12-03 20:33:03 -0300

    Classes deletadas. Não serão persistidas.

--- src/main/java/br/com/ufsm/csi/pilacoin/repository/QueryRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.ufsm.csi.pilacoin.repository;
-
-import br.com.ufsm.csi.pilacoin.model.Query;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QueryRepository extends JpaRepository<Query, Long> {
-}
