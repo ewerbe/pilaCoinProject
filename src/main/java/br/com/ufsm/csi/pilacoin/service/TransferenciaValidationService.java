28322d909071e988757a24bc5bc79575c39e1593
ewerbe <ewertonbarbos@gmail.com>
2023-12-03 12:07:04 -0300

    Classe iniciada.

---
+++ src/main/java/br/com/ufsm/csi/pilacoin/service/TransferenciaValidationService.java
@@ -0,0 +1,10 @@
+package br.com.ufsm.csi.pilacoin.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TransferenciaValidationService {
+
+    //listar usuarios;
+    //transferir pilacoin;
+}
