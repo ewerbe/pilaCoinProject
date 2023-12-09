f1dd09a796df6c97f4ece20f5cfda2fc1a8cff3c
ewerbe <ewertonbarbos@gmail.com>
2023-12-09 15:00:40 -0300

    Implementado findById.

--- src/main/java/br/com/ufsm/csi/pilacoin/service/UsuarioService.java
+++ src/main/java/br/com/ufsm/csi/pilacoin/service/UsuarioService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UsuarioService {
@@ -23,5 +24,9 @@ public class UsuarioService {
 
     public void saveAll(List<Usuario> usuariosList) {
         usuarioRepository.saveAll(usuariosList);
+    }
+
+    public Optional<Usuario> findById(Long idUsuario) {
+        return usuarioRepository.findById(idUsuario);
     }
 }
